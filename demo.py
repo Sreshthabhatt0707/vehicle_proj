3f619a6f17c43f7671205b54b61e214c41489385
Sreshtha Bhatt <bhattsreshtha@gmail.com>
2025-09-15 16:52:31 +0530

    Project Complete.

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # below code is to check the logging config
 from src.logger import logging
 
-logging.debug("This is a debug message.")
+# logging.debug("This is a debug message.")
 # logging.info("This is an info message.")
 # logging.warning("This is a warning message.")
 # logging.error("This is an error message.")
@@ -22,7 +22,7 @@ logging.debug("This is a debug message.")
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
